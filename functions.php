3202a76d8057921863882c50b574ce5d845555b9
axelsundin <axelsundin@hotmail.com>
2021-04-14 16:37:51 +0200

    Dag 2
    Lagt in stöd för menyer och widgets + potentiellt användbar kod  för sidebar och widgets

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 
 add_theme_support( 'post-thumbnails' );
+add_theme_support( 'menus' );
+add_theme_support( 'widgets' );
 
+// get_sidebar(); // tänk get_header() eller get_footer() ...
+
+/* register_sidebar(); // skapar område att skapa widget på
+
+dynamic_sidebar(); // tänk wp_nav_menu() */
 
 ?>
