55a6a5a970b643a1050ea339b00fd584baca3d1e
axelsundin <axelsundin@hotmail.com>
2021-04-23 15:15:00 +0200

    index.php
    fått med main, section, divar osv som matchar sidans tema
    sidan skriver ut "innehåll saknas" om poster ej finns

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 get_header(); //Skriver ut header.php
 
+?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div id="primary" class="col-xs-12 col-md-9">
+                    <article>
+
+<?php
+
 if ( have_posts() ) { // kollar om anropet innehåller posts
 
     while ( have_posts() ) { //loopar över posts om de finns
@@ -11,7 +22,22 @@ if ( have_posts() ) { // kollar om anropet innehåller posts
         the_content(); // skriver ut postens innehåll
     }
 
+} else {
+    
+    echo '<h1>Innehåll saknas</h1>';
+
 }
+
+?>
+
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
 
 get_footer(); //Skriver ut footer.php
 
